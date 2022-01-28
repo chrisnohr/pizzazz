206cdfc0a278aede0d739c06e9d3fe080120fa6f
Chris Nohr <chris@thenumber.ca>
2022-01-28 10:45:16 -0800

    count submisisons

--- client/src/gameFlow/solutions/solutionsState.js
+++ client/src/gameFlow/solutions/solutionsState.js
@@ -1,8 +1,9 @@
 import { START_GAME } from '../../gameFlow/gameFlowState';
-import { take, put } from 'redux-saga/effects';
+import { take, put, select } from 'redux-saga/effects';
 import { messageAction } from '../../Message/messageState';
 
 export const NEW_SOLUTION = 'NEW_SOLUTION';
+const getSolutions = state => state.solutions;
 
 export const solutionsReducer = (state = [], action) => {
   switch (action.type) {
@@ -21,7 +22,6 @@ export const newSolutionAction = solution => ({
 });
 
 export const subscribeSolutions = (socket, emit) => {
-  console.log('yeah')
   socket.on('newSolution', solution => emit(newSolutionAction(solution)));
 };
 
@@ -29,8 +29,10 @@ export const subscribeSolutions = (socket, emit) => {
 export function* displaySolution() {
   while (true) {
     const { solution } = yield take(NEW_SOLUTION);
+    const solutions = yield select(getSolutions);
+
     yield put(messageAction(
-      `${solution.name.toUpperCase()} played a ${solution.solution.length}-letter word`,
+      `${solution.name.toUpperCase()} played a ${solution.solution.length}-letter word (${solutions.length})`,
     ));
   }
 }
